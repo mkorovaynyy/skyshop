da1c1130a7b09ca6dd436e590411cdffe4649755
progTD <corowainy@yandex.ru>
2025-02-20 20:07:18 +0300

    dz1

---
+++ src/main/org.skypro.skyshop/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,62 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    // Массив для хранения продуктов (максимум 5 элементов)
+    private Product[] products = new Product[5];
+
+    // Метод добавления продукта в корзину
+    public void addProduct(Product product) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                products[i] = product;
+                return;
+            }
+        }
+        System.out.println("Невозможно добавить продукт");
+    }
+
+    // Метод получения общей стоимости корзины
+    public int getTotalCost() {
+        int totalCost = 0;
+        for (Product product : products) {
+            if (product != null) {
+                totalCost += product.getPrice();
+            }
+        }
+        return totalCost;
+    }
+
+    // Метод печати содержимого корзины
+    public void printBasket() {
+        if (products[0] == null) {
+            System.out.println("В корзине пусто");
+            return;
+        }
+
+        for (Product product : products) {
+            if (product != null) {
+                System.out.println(product.getName() + ": " + product.getPrice());
+            }
+        }
+        System.out.println("Итого: " + getTotalCost());
+    }
+
+    // Метод проверки наличия продукта в корзине по имени
+    public boolean containsProduct(String productName) {
+        for (Product product : products) {
+            if (product != null && product.getName().equals(productName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Метод очистки корзины
+    public void clearBasket() {
+        for (int i = 0; i < products.length; i++) {
+            products[i] = null;
+        }
+    }
+}
--- src/main/org.skypro.skyshop/org/skypro/skyshop/product/Product.java
+++ src/main/org.skypro.skyshop/org/skypro/skyshop/product/Product.java
@@ -1,4 +1,41 @@
 package org.skypro.skyshop.product;
 
+import java.util.Objects;
+
 public class Product {
+    private String Name;
+    private Integer Price;
+
+    public Product(String productName, Integer productValue) {
+        this.Name = productName;
+        this.Price = productValue;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public Integer getPrice() {
+        return Price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(Name, product.Name) && Objects.equals(Price, product.Price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Name, Price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + Name + '\'' +
+                ", productValue=" + Price +
+                '}';
+    }
 }
